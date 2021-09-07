f0dd0042e20580675b183eff8c7224113f3508dd
Rob Pupo <pupo.robert@gmail.com>
2021-09-07 14:08:16 -0400

    initial server.js with requirements

---
+++ server.js
@@ -0,0 +1,11 @@
+const consoleTable = require("console.table");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employeetracker",
+});

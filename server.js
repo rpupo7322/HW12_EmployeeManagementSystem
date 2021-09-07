52a698808774eb1187c22ee70e2239e2d64d79d1
Rob Pupo <pupo.robert@gmail.com>
2021-09-07 15:09:26 -0400

    bringing queries back to server.js - error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const consoleTable = require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+// const query = require('./libraries/queries');
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -73,6 +74,113 @@ const questions = [
     },
   ];
 
+
+  
+
+const viewEmployees = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT e.id as ID
+        , CONCAT(e.first_name, ' ', e.last_name) AS Employee
+        , r.title AS Title
+        , d.name AS Department
+        , r.salary AS Salary
+        , CONCAT(m.first_name,' ', m.last_name) AS Manager 
+        FROM employee e 
+            LEFT JOIN employee m ON m.id = e.manager_id 
+            JOIN role r ON e.role_id = r.id 
+            JOIN department d ON d.id = r.department_id 
+            ORDER BY e.last_name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+
+const viewEmployeesDepartment = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT e.id as ID
+        , CONCAT(e.first_name, ' ', e.last_name) AS Employee
+        , r.title AS Title
+        , d.name AS Department
+        , r.salary AS Salary
+        , CONCAT(m.first_name,' ', m.last_name) AS Manager 
+        FROM employee e 
+            LEFT JOIN employee m ON m.id = e.manager_id 
+            JOIN role r ON e.role_id = r.id 
+            JOIN department d ON d.id = r.department_id 
+            ORDER BY d.name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+const viewEmployeesManager = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT e.id as ID
+        , CONCAT(e.first_name, ' ', e.last_name) AS Employee
+        , r.title AS Title
+        , d.name AS Department
+        , r.salary AS Salary
+        , CONCAT(m.first_name,' ', m.last_name) AS Manager 
+        FROM employee e 
+            LEFT JOIN employee m ON m.id = e.manager_id 
+            JOIN role r ON e.role_id = r.id 
+            JOIN department d ON d.id = r.department_id 
+            ORDER BY m.last_name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+  
+const viewRoles = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT r.id as ID
+        , r.title AS Role
+        FROM role r 
+            ORDER BY r.title ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+
+const viewDepartments = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT d.id as ID
+        , d.name AS Department
+        FROM department d 
+            ORDER BY d.name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
 
 init = () => {
     inquirer.prompt(questions).then((answer) => {

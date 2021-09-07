b6d3c08969cffdb5fb09bb24ac4bddbbf6a8a1ce
Rob Pupo <pupo.robert@gmail.com>
2021-09-07 14:56:11 -0400

    all read queries added to server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const questions = [
     {
       type: "list",
       message: "What would you like to do?",
-      name: "layer1",
+      name: "entryQuestion",
       choices: [
         {
           name: "View All Employees",
@@ -24,6 +24,10 @@ const questions = [
       {
         name: "View All Employees By Department",
         value: "employeeDepartmentView",
+      },
+      {
+        name: "View All Employees By Manager",
+        value: "employeeManagerView",
       },
         {
           name: "Add Employee",
@@ -94,16 +98,109 @@ const viewEmployees = () => {
   )
 };
 
-// }
+const viewEmployeesDepartment = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT e.id as ID
+        , CONCAT(e.first_name, ' ', e.last_name) AS Employee
+        , r.title AS Title
+        , d.name AS Department
+        , r.salary AS Salary
+        , CONCAT(m.first_name,' ', m.last_name) AS Manager 
+        FROM employee e 
+            LEFT JOIN employee m ON m.id = e.manager_id 
+            JOIN role r ON e.role_id = r.id 
+            JOIN department d ON d.id = r.department_id 
+            ORDER BY d.name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+const viewEmployeesManager = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT e.id as ID
+        , CONCAT(e.first_name, ' ', e.last_name) AS Employee
+        , r.title AS Title
+        , d.name AS Department
+        , r.salary AS Salary
+        , CONCAT(m.first_name,' ', m.last_name) AS Manager 
+        FROM employee e 
+            LEFT JOIN employee m ON m.id = e.manager_id 
+            JOIN role r ON e.role_id = r.id 
+            JOIN department d ON d.id = r.department_id 
+            ORDER BY m.last_name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
   
+const viewRoles = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT r.id as ID
+        , r.title AS Role
+        FROM role r 
+            ORDER BY r.title ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+
+const viewDepartments = () => {
+    console.log(`Loading all information... \n`);
+  connection.query(
+    `SELECT d.id as ID
+        , d.name AS Department
+        FROM department d 
+            ORDER BY d.name ASC`, 
+    (err, res) => {
+      if(err) {
+        console.error(`Error : `, err); 
+      }
+      console.table(res); 
+      init(); 
+    }
+  )
+};
+
 init = () => {
     inquirer.prompt(questions).then((answer) => {
         switch (answer.entryQuestion) {
             case "employeesView":
                 viewEmployees();
-              break;
+            break;
+            case "employeeDepartmentView":
+                viewEmployeesDepartment();
+            break;
+            case "employeeManagerView":
+                viewEmployeesManager();
+            break;
+            case "roleView":
+                viewRoles();
+            break;
+            case "departmentView":
+                viewDepartments();
+            break;
     }
 })};
+
+
 
 connection.connect((err) => {
     if (err) throw err;
